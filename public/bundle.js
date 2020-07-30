657df5afda0fc5738a74e27dde970eb9d67991c0
andyprasetya <andyprasetya@opense7en.com>
2020-07-30 16:50:00 +0700

    sample landing page

---
+++ public/bundle.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  /* -- code -- */
+
+  /* -- /code -- */
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length > 0) {
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        });
+      });
+    }
+  });
+  document.querySelectorAll('.navbar-link').forEach(function (navbarLink) {
+    navbarLink.addEventListener('click', function () {
+      navbarLink.nextElementSibling.classList.toggle('is-hidden-mobile');
+    });
+  });
+
+}());
+//# sourceMappingURL=bundle.js.map
---
+++ src/arsip/halaman-beranda.js
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<head lang='en'>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='icon' href='./img/favicon.ico'>
+  <title>Title</title>
+  <link rel='stylesheet' href='./bundle.css'>
+</head>
+<body>
+  <nav class='navbar is-dark' role='navigation' aria-label='main navigation'>
+    <div class='navbar-brand'>
+      <a class='navbar-item' href='./'>
+        SIG Persampahan Kota Tegal
+      </a>
+  
+      <a role='button' class='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
+        <span aria-hidden='true'></span>
+        <span aria-hidden='true'></span>
+        <span aria-hidden='true'></span>
+      </a>
+    </div>
+  
+    <div id='navbarBasicExample' class='navbar-menu'>
+      <div class='navbar-start'>
+        <a class='navbar-item'>
+          Beranda
+        </a>
+  
+        <a class='navbar-item'>
+          Data
+        </a>
+  
+        <div class='navbar-item has-dropdown is-hoverable'>
+          <a class='navbar-link'>
+            Peta
+          </a>
+  
+          <div class='navbar-dropdown'>
+            <a class='navbar-item'>
+              Batas Wilayah Administrasi
+            </a>
+            <a class='navbar-item'>
+              TPS/TPA
+            </a>
+          </div>
+        </div>
+      </div>
+  
+      <div class='navbar-end'>
+        <div class='navbar-item'>
+          <div class='buttons'>
+            <a class='button is-primary'>
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div id='app'>
+
+    <section class='section'>
+      <div class='container'>
+        <div class='columns'>
+          <div class='column'>
+            <div class='box'>
+              <article class='media'>
+                <div class='media-left'>
+                  <figure class='image is-64x64'>
+                    <h1 class='is-danger'>25</h1>
+                  </figure>
+                </div>
+                <div class='media-content'>
+                  <div class='content'>
+                    <p>
+                      <strong>TPS</strong>
+                      <br>
+                      Tempat Pembuangan Sementara
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class='column'>
+            <div class='box'>
+              <article class='media'>
+                <div class='media-left'>
+                  <figure class='image is-64x64'>
+                    <h1 class='is-danger'>15</h1>
+                  </figure>
+                </div>
+                <div class='media-content'>
+                  <div class='content'>
+                    <p>
+                      <strong>Alat</strong>
+                      <br>
+                      Alat pendukung persampahan Kota Tegal
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class='column'>
+            <div class='box'>
+              <article class='media'>
+                <div class='media-left'>
+                  <figure class='image is-64x64'>
+                    <h1 class='is-danger'>27</h1>
+                  </figure>
+                </div>
+                <div class='media-content'>
+                  <div class='content'>
+                    <p>
+                      <strong>Kelurahan</strong>
+                      <br>
+                      Wilayah kelurahan di Kota Tegal
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class='column'>
+            <div class='box'>
+              <article class='media'>
+                <div class='media-left'>
+                  <figure class='image is-64x64'>
+                    <h1 class='is-danger'>4</h1>
+                  </figure>
+                </div>
+                <div class='media-content'>
+                  <div class='content'>
+                    <p>
+                      <strong>Kecamatan</strong>
+                      <br>
+                      Wilayah Kecamatan di Kota Tegal
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class='section' style='padding-top:0;'>
+      <div class='container'>
+        <figure class="image is-5by3">
+          <img src="https://bulma.io/images/placeholders/128x128.png">
+        </figure>
+      </div>
+    </section>
+
+    <section class='section' style='padding-top:0;'>
+      <div class='container'>
+        <div class='box'>
+          <article class='media'>
+            <div class='media-content'>
+              <div class='content'>
+                <p>&copy;&nbsp;2020 - Tim Developer SIG Persampahan Kota Tegal</p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  </div>
+  <script src='./bundle.js'></script>
+</body>
+</html>
